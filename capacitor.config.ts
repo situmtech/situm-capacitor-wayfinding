ef6aff1e41754d7742a1130e89a84ae51eb8a8e1
Fernando Sanchez <fernando.sanchez@situm.es>
2022-04-21 18:47:31 +0200

    Customized app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'situm-capacitor-wayfinding',
+  appId: 'es.situm.capacitor.wayfinding',
+  appName: 'Situm Wayfinding',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SitumWayfinding } from 'situm-capacitor-plugin-wayfinding';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,34 @@ export class HomePage {
 
   constructor() {}
 
+  // LibrarySettings:
+  readonly librarySettings = {
+    user: "situm_user",
+    apiKey: "situm_apikey",
+    iosGoogleMapsApiKey: "ios_goole_maps_apikey",
+    buildingId : "building_id",
+    dashboardUrl: "https://dashboard.situm.com",
+    hasSearchView: true,
+    searchViewPlaceholder: "Capacitor WYF",
+    useDashboardTheme: false,
+  };
+
+
+  //When the app is loaded
+  ionViewDidEnter(){
+    this.loadSitumWayfinding('situm-map')
+  }
+
+  //Load Situm Wayfinding Module 
+  async loadSitumWayfinding(targetId: string) {
+    const element: HTMLElement | null = document.getElementById(targetId);
+    if (!element) {
+      return;
+    }
+    
+    try {
+      const wyfResponse = await SitumWayfinding.load(element, this.librarySettings);
+    } catch (e) {
+    }
+  }
 }
