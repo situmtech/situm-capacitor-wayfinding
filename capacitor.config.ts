37958f3f2739b63eedb6bd3f567ad4488ad33880
Fernando Sanchez <fernando.sanchez@situm.es>
2022-04-21 12:36:42 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'situm-capacitor-wayfinding',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
